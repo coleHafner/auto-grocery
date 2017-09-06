06b2836934cdd67351e7f8581c142108c61fad49
Cole Hafner (Personal MacBook) <colehafner@gmail.com>
2017-09-05 22:13:58 -0700

    Added support for allrecipes.com

--- src/services/recipe-scraper.js
+++ src/services/recipe-scraper.js
@@ -6,6 +6,7 @@ import appStorage from 'services/app-storage';
 $(function() {
     var url = window.location.href,
         SRC_BLUEAPRON = 'blueapron',
+        SRC_ALLRECIPES = 'allrecipes',
         recipe = {
             src: '',
             url: '',
@@ -24,36 +25,69 @@ $(function() {
         },
         cleanText = function(text) {
             return text ? text.replace(/(\r\n|\n|\r)/gm, ' ').trim().replace('  ', ' ') : '';
-        };
+        },
+        scrapeBlueApron = function(recipe) {
+            // set src
+            recipe.src = SRC_BLUEAPRON;
+
+            // set title
+            recipe.title = cleanText($('.ba-recipe-title__main').text());
+
+            // set ingredients
+            $('.ba-info-list--stacked li').each(function() {
+                var div = $(this).find('div.non-story'),
+                    name;
+
+                //qty and unit
+                var qtyStr = cleanText($(this).find('span').text()),
+                    split = qtyStr.split(' '),
+                    qtyUnit = split[1],
+                    qty = split[0];
+
+                // ingredient name
+                if(div.length) {
+                    name = cleanText(div.text()).replace(qty, '').replace(qtyUnit, '').trim();
+                }else {
+                    name = $(this).find('a').attr('data-ingredient');
+                }
+
+                addIngredient(recipe, name, qty, qtyUnit);
+            });
+        },
+        scrapeAllRecipes = function(recipe) {
+            // set src
+            recipe.src = SRC_ALLRECIPES;
+            recipe.title = $('.recipe-summary__h1').text();
+            
+            let listExists = true,
+                listNum = 1;
+
+            while (listExists) {
+                let listClass = `.list-ingredients-${listNum}`;
+                if (!$(listClass.length)) {
+                    listExists = false;
+                }
+                $(`${listClass} li span.recipe-ingred_txt`).each(function(ingr) { 
+                    let ingrText = $(this).text(),
+                        ingrSplit = ingrText.split(' '),
+                        qty = ingrSplit.shift(),
+                        qtyUnit = ingrSplit[1].toLowerCase() === 'bunch' ? `${ingrSplit.shift()} ${ingrSplit.shift()}` : ingrSplit.shift(),
+                        name = ingrSplit.join(' ');
+
+                    addIngredient(recipe, name, qty, qtyUnit);
+                });
+
+                listNum++;
+            }
+        }
 
     recipe.url = url;
 
     if (/blueapron.com\/recipes\/.+$/.test(url) === true) {
-        recipe.src = SRC_BLUEAPRON;
+        scrapeBlueApron(recipe);
 
-        // set title
-        recipe.title = cleanText($('.ba-recipe-title__main').text());
-
-        // set ingredients
-        $('.ba-info-list--stacked li').each(function() {
-            var div = $(this).find('div.non-story'),
-                name;
-
-            //qty and unit
-            var qtyStr = cleanText($(this).find('span').text()),
-                split = qtyStr.split(' '),
-                qtyUnit = split[1],
-                qty = split[0];
-
-            // ingredient name
-            if(div.length) {
-                name = cleanText(div.text()).replace(qty, '').replace(qtyUnit, '').trim();
-            }else {
-                name = $(this).find('a').attr('data-ingredient');
-            }
-
-            addIngredient(recipe, name, qty, qtyUnit);
-        });
+    } else if (/allrecipes.com\/recipe\/.+$/.test(url) === true) {
+        scrapeAllRecipes(recipe);
     }
 
     console.log('recipe', recipe);
