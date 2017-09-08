da0880df3b21dae8cae2ed6f89a88695e4bbf001
Cole Hafner (Personal MacBook) <colehafner@gmail.com>
2017-09-07 23:58:05 -0700

    Started tests for recipe parser. Added support for multi-character fractions.

--- src/services/recipe-parser.js
+++ src/services/recipe-parser.js
@@ -7,12 +7,21 @@ export default {
             ? `${ingr.name} ${delim}`
             : `${ingr.name} ${delim} ${qtyStr}`
     },
-    getTotalQty(qty, curQty) {
+    getTotalQty(qty, curQty = 0) {
         let ingrQtyCopy = qty;
 
         if (qty.indexOf('/') > -1) {
-            let qtySplit = qty.split('/'),
-                numerator = parseInt(qtySplit[0]),
+            let qtySplit = qty.split('/');
+
+            if (qty.indexOf(' ') > -1) {
+                let fracSplit = qty.split(' '),
+                    qtySplit = fracSplit[1].split('/');
+
+                // add whole number to total
+                curQty += parseInt(fracSplit[0]);
+            }
+
+            let numerator = parseInt(qtySplit[0]),
                 denominator = parseInt(qtySplit[1]);
 
             ingrQtyCopy = numerator/denominator;
@@ -21,20 +30,21 @@ export default {
             for (var i = 0, len = qty.length; i < len; ++i) {
                 let char = qty.charAt(i),
                     charCode = qty.charCodeAt(i),
-                    foundFraction = [188, 189, 190, 8539].indexOf(charCode) > -1;
+                    foundFraction = [188, 189, 190, 8539, 8531, 8532].indexOf(charCode) > -1;
 
                 if (foundFraction) {
                     if (charCode === 8539) {
                         curQty += .125;
-
                     } else if (charCode === 188) {
                         curQty += .25;
-
                     } else if (charCode === 189) {
                         curQty += .5;
-
                     } else if (charCode === 190) {
                         curQty += .75;
+                    } else if (charCode === 8531) {
+                        curQty += .33;
+                    } else if (charCode === 8532) {
+                        curQty += .66;
                     }
                     ingrQtyCopy = ingrQtyCopy.replace(char, '');
                 }
--- test/recipe-parser.test.js
+++ test/recipe-parser.test.js
@@ -10,13 +10,42 @@ let should = chai.should(),
 describe('Recipe Parser', () => {
     it ('getTotalQty - it should detect and convert multi-character fractions', () => {
         let cases = [
-            ['1/4', .25]
+            ['1/4', .25],
+            ['1 1/4', 1.25]
         ];
 
         cases.forEach(curCase => {
-            let actual = recipeParser.getTotalQty(curCase[0]);
+            let actual = recipeParser.default.getTotalQty(curCase[0]);
             assert.equal(actual, curCase[1])
         });
-        
+    });
+
+    it ('getTotalQty - it should detect and convert unicode fractions', () => {
+        let cases = [
+            ['¼', .25],
+            ['½', .5],
+            ['⅓', .33],
+            ['⅔', .66],
+            ['¼', .25],
+            ['¾', .75],
+            // ['⅕', .2],
+            // ['⅖', .4],
+            // ['⅗', .6],
+            // ['⅘', .8],
+            // ['⅙', .125],
+            // ['⅚', .5],
+            // ['⅐', .5],
+            // ['⅛', .5],
+            // ['⅜', .5],
+            // ['⅝', .5],
+            // ['⅞', .5],
+            // ['⅑', .5],
+            // ['⅒', .5]
+        ];
+
+        cases.forEach(curCase => {
+            let actual = recipeParser.default.getTotalQty(curCase[0]);
+            assert.equal(actual, curCase[1])
+        });
     });
 });
